42d37299cccc1c1e59b49f8251d21841b110671d
Vittorio nuvolari <vittorio.nuvolari@galileiostiglia.edu.it>
2024-09-25 14:07:05 +0000

    es1

---
+++ 3I/INFORMATICA/Compiti/es1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float studenti,sconto
+    printf("Inserisci numero studenti");
+    scanf("%f",&studenti);
+    sconto=(studenti*12)-50%
+    printf(sconto)
+}
