083c8ded904c193a645f552b168d248e36f12d72
Vittorio nuvolari <vittorio.nuvolari@galileiostiglia.edu.it>
2024-10-01 05:57:33 +0000

    es2 01 10 24

---
+++ 3I/INFORMATICA/Compiti/es2_01_10_24.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    float num1=0,num2=0,num3=0,min=0,max=0,media=0;
+    printf("Inserisci il primo numero: \t");
+    scanf("%f",&num1);
+    printf("Inserisci il secondo numero: \t");
+    scanf("%f",&num2);
+    printf("Inserisci il terzo numero: \t");
+    scanf("%f",&num3);
+     min = num1;
+    if (num2 < min) 
+    {
+        min = num2;
+    }
+    if (num3 < min) 
+    {
+        min = num3;
+    }
+     max = num1;
+    if (num2 > max) 
+    {
+        max = num2;
+    }
+    if (num3 > max) 
+    {
+        max = num3;
+    }
+     media = (num1 + num2 + num3) / 3;
+    printf("Il numero più piccolo è: %.2f\n", min);
+    printf("Il numero più grande è: %.2f\n", max);
+    printf("La media aritmetica è: %.2f\n", media);
+
+}
