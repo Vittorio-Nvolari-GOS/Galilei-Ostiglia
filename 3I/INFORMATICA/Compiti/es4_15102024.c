8f071aea967f6344ddf2ff458b375fd5486f1494
Vittorio nuvolari <vittorio.nuvolari@galileiostiglia.edu.it>
2024-10-15 11:25:14 +0000

    oke

---
+++ 3I/INFORMATICA/Compiti/es4_15102024.c
@@ -0,0 +1,24 @@
+/*Memorizzare il seme e il punteggio di due carte da poker. Dire quale delle carte vale di più. 
+ Il valore di una carta da poker è dato in primo luogo dal punteggio e in caso di parità di punteggio dal seme.
+I punteggi in ordine decrescente sono {k,q,j,9,8,7,6,5,4,3,2,1}, i semi in ordine decrescente sono 
+ {cuori, quadri, fiori, picche} per i semi memorizzare solo la lettera iniziale del seme.
+PS: l’ordine dei semi può essere ricordato grazie alla frase “come quando fuori piove” in
+ cui le parole ricordano i nomi dei semi (cuori, quadri, fiori, picche).*/
+
+
+#include <stdio.h>
+int main()
+{
+    int seme1=0,seme2=0,carta1=0,carta2=0,k=13,q=12,j=11;
+    prinf("Inserisci il seme della prima carta: \t");
+    scanf("%d",&seme1);
+    printf("Insersci il numero o simbolo della prima carta: \t");
+    scanf("%f",&carta1);
+    prinf("inserisci il seme della seconda carta: \t");
+    scanf("%d",&seme2);
+    printf("Inserisci il numero o simbolo della seconda carta: \t");
+    scanf("%d",&carta2);
+    if()
+
+
+}
