5fb613b03dd76f139220cc0566b0db241b5cf361
Vittorio nuvolari <vittorio.nuvolari@galileiostiglia.edu.it>
2025-02-06 19:35:49 +0000

    Calcolo circonferenza e cerchio

--- 3I/INFORMATICA/es2_06_02_2025.c
+++ 3I/INFORMATICA/es2_06_02_2025.c
@@ -3,27 +3,36 @@
 
 #include<stdio.h>
 
-void conf(float *_raggio ,float *_diametro);
-
-void area(float *_raggio,float *_area);
 const float pigreco=3.14;
+
+void conf(float *_raggio ,float *_conf);
+
+void Area(float *_raggio,float *_area);
 
 
 int main()
 {
    float raggio=0,circonferenza=0,area=0;
 
-   prinft("Inserisci il valore del raggio:\t");
-   scanf("%d", &raggio);
+   printf("Inserisci il valore del raggio:\t");
+   scanf("%f", &raggio);
 
    conf(&raggio,&circonferenza);
 
-   area(&raggio,&area);
+   Area(&raggio,&area);
+
+   printf("La circonferenza del cerchio è %.2f\n",circonferenza);
+   printf("L'area del cerchio è %.2f\n",area);
 
 
-}   
+}  
 
 void conf(float *_raggio ,float *_conf)
 {
     *_conf=*_raggio*2*pigreco;
+}
+
+void Area(float *_raggio,float *_area)
+{
+    *_area=(*_raggio**_raggio)*pigreco;
 }
