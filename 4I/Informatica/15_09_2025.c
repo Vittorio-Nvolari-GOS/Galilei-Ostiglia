7cb304a63c615a14272f2b427a9c7a19c1e69bf1
Vittorio nuvolari <vittorio.nuvolari@galileiostiglia.edu.it>
2025-09-15 22:01:53 +0000

    Primo es

---
+++ 4I/Informatica/15_09_2025.c
@@ -0,0 +1,44 @@
+//Data una matrice di valori scambiare gli elementi della diagonale principale con la diagonale secondaria.
+#include <stdlib.h>
+#define D 4
+
+
+int main()
+{
+    int m1[D][D]=
+    {
+        {2,4,8,12},
+        {9,6,10,3},
+        {61,8,3,5},
+        {0,1,7,11}
+    };
+    int temp=0;
+    int i=0;
+    int j=0;
+
+
+    printf("Matrice originale: \n");
+    for (i = 0; i < D; i++) {
+        for (j = 0; j < D; j++) {
+            printf("%3d ", m1[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    for (i = 0; i < D; i++) {
+        temp = m1[i][i];                       
+        m1[i][i] = m1[i][D - 1 - i];          
+        m1[i][D - 1 - i] = temp;
+    }
+
+    printf("\nMatrice dopo lo scambio delle diagonali:\n");
+    for (i = 0; i < D; i++) {
+        for (j = 0; j < D; j++) {
+            printf("%3d ", m1[i][j]);
+        }
+        printf("\n");
+    }
+
+
+}
